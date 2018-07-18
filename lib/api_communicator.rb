2060036cd95c040219ebee6e6f947a948ecfd27c
joshua.miles <joshua.miles@sf-053349-mbp.local>
2018-07-18 15:29:25 -0500

    Improved Variable Names

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,10 +4,10 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
   
-  # iterate over the character hash to find the collection of `films` for the given
+  # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
