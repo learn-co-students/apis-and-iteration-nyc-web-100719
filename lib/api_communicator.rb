950b2efec264ad2854c4e37d7a7e31e5e184ca46
dahby <dstoll243@gmail.com>
2019-05-15 10:18:00 -0700

    demo solution from lecture

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,23 +3,30 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
+
+  # make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  
+  # Find the matching character
+  all_characters = response_hash["results"]
+  found_character = all_characters.find do |character|
+    character["name"].downcase == character_name
+  end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  # map through the found characters films, get request on each url, return parsed film data
+  found_character["films"].map do |film|
+    film_data = RestClient.get(film)
+    JSON.parse(film_data)
+  end
+  
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each.with_index do |film, i|
+    puts "#{i + 1} - #{film["title"]}"
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
 def welcome
   # puts out a welcome message here!
+  puts "HERE IS A WELCOME MESSAGE"
+
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  character = gets.chomp.downcase
+  character
   # use gets to capture the user's input. This method should return that input, downcased.
 end
